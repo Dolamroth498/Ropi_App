12678b94ce0e27b431d098c58b09f3fc0ff3fb3e
Zsombor <dolamroth498@gmail.com>
2025-04-27 14:43:48 +0200

    Első commit – Android app

---
+++ app/src/main/java/com/example/ropiapp/BajnoksagActivity.java
@@ -0,0 +1,39 @@
+package com.example.ropiapp;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class BajnoksagActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = BajnoksagActivity.class.getName();
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_bajnoksag);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            Log.d(LOG_TAG, "Bejelentkezett felhasználó!");
+        }else{
+            Log.d(LOG_TAG, "Nem bejelentkezett felhasználó!");
+            finish();
+        }
+    }
+}
